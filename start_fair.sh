5fd9a57ceb40921f8c50d2fd5452d04cb2511478
carl rogers <carl.rogers@gmail.com>
2019-02-15 17:07:09 -0600

    start_fair.sh more scripted

--- start_fair.sh
+++ start_fair.sh
@@ -2,15 +2,28 @@
 
 tmux new-session -d -s scheduler
 tmux setw remain-on-exit on
-tmux new-window -t scheduler:1 'ssh node1 -N -R 5055:localhost:5055'
-tmux setw remain-on-exit on
-tmux new-window -t scheduler:2 'ssh node1 -N -R 5056:localhost:5056'
-tmux setw remain-on-exit on
-tmux new-window -t scheduler:3 'ssh node1 -N -R 5057:localhost:5057'
-tmux setw remain-on-exit on
-tmux new-window -t scheduler:4 'ssh node1 -N -R 5058:localhost:5058'
-tmux setw remain-on-exit on
-tmux new-window -t scheduler:5 'ssh node1 -t "python -m scheduler.fair_scheduler 12"'
-tmux setw remain-on-exit on
-tmux new-window -t scheduler:6 'python -m scheduler.fair_scheduler 4'
-tmux setw remain-on-exit on
+
+declare -a NODES=(node1 node2)
+declare -a PORTS=(5055 5056 5057 5058)
+
+#SSH TUNNELS
+n=1
+for node in ${NODES[@]}; do
+    for port in ${PORTS[@]}; do
+        tmux new-window -t scheduler:$n "ssh $node -N -R $port:localhost:$port"
+        tmux setw remain-on-exit on
+        tmux join-pane -s $((n+1)) -t $n
+    done
+    tmux select-layout tiled
+    n=$((n+1))
+done
+
+#START WORKERS
+for node in ${NODES[@]}; do
+    tmux new-window -t scheduler "ssh $node -t 'python -m scheduler.fair_scheduler 12'"
+    tmux setw remain-on-exit on
+done
+
+#LOCALHOST WORKER
+#tmux new-window -t scheduler:9 'python -m scheduler.fair_scheduler 4'
+#tmux setw remain-on-exit on
